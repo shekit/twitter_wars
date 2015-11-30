6be686c53de3a74175c8377a1a05d0e1dda9b703
shekit <as7790@nyu.edu>
2015-11-29 19:56:48 -0500

    selecting contestants

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 	console.log('hello')
+
+	var contestantCount = 0;
+
+	$(".contestant").on('click', function(event){
+		event.preventDefault();
+
+		if(contestantCount < 2){	
+			contestantCount++;
+			console.log('Contestant Count: ', contestantCount);
+
+			var value = $(this).attr('data-val')
+
+			if(contestantCount==1){
+				$(this).addClass('contestant-one')
+			} else if(contestantCount == 2){
+				$(this).addClass('contestant-two')
+			}
+			console.log(value)
+		} else {
+			console.log('TWO CONTESTANTS SELECTED')
+		}
+	})
 })
