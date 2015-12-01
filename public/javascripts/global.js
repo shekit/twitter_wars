2221541fb1d24a9b40c8e944ec3571d953fe191d
shekit <as7790@nyu.edu>
2015-12-01 01:29:36 -0500

    added basic winner prediction mechanism

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
 
 	var contestantCount = 0;
 
+	var contestantOne = null;
+	var contestantTwo = null;
+
+	var contestantOneScore = 0;
+	var contestantTwoScore = 0;
+
+	var prediction = null;
+
 	$(".contestant").on('click', function(event){
 		event.preventDefault();
 
@@ -29,9 +37,10 @@ $(document).ready(function(){
 	$("#fight").on('click', function(event){
 		event.preventDefault();
 		if(contestantCount == 2){
-			var contestantOne = $(".contestant-one").attr('data-val');
-			var contestantTwo = $(".contestant-two").attr('data-val');
+			contestantOne = $(".contestant-one").attr('data-val');
+			contestantTwo = $(".contestant-two").attr('data-val');
 			socket.emit('contestants',{'contestantOne':contestantOne,'contestantTwo': contestantTwo})
+			updatePredictionValues();
 		} else {
 			alert('select contestants')
 		}
@@ -59,6 +68,13 @@ $(document).ready(function(){
 		}*/
 	})
 
+	$("body").on('click','.bet', function(event){
+		event.preventDefault();
+
+		prediction = $(this).attr('data-val')
+		console.log(prediction)
+	})
+
 	$("#socket-fight").on('click', function(event){
 		event.preventDefault();
 		socket.emit('fight','yes')
@@ -66,6 +82,39 @@ $(document).ready(function(){
 
 	$("#socket-stop-fight").on('click', function(event){
 		event.preventDefault();
-		socket.emit('stop','yes')
+		socket.emit('stop','yes');
+		checkWinner();
 	})
+
+	socket.on('one', function(msg){
+		contestantOneScore++;
+	})
+
+	socket.on('two', function(msg){
+		contestantTwoScore++;
+
+	})
+
+	function updatePredictionValues(){
+		console.log("update")
+		$(".bet-one").html(contestantOne);
+		$(".bet-two").html(contestantTwo);
+	}
+
+	function checkWinner(){
+		if(contestantOneScore > contestantTwoScore && prediction == 'contestantOne'){
+			alert('you win contestant one won');
+		} else if(contestantOneScore < contestantTwoScore && prediction == 'contestantOne') {
+			alert('you lose contestant one lost')
+		} else if(contestantTwoScore > contestantOneScore && prediction == 'contestantTwo') {
+			alert('you win, contestant two won')
+		} else if(contestantTwoScore < contestantOneScore && prediction == 'contestantTwo') {
+			alert('you lose contestant two lost')
+		} else {
+			alert('its a tie!')
+		}
+
+		console.log('CONTESTANT ONE SCORE: '+contestantOneScore);
+		console.log('CONTESTANT TWO SCORE: '+contestantTwoScore)
+	}
 })
